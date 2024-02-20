aa5c400d4c993c8d8794e4a8e435cee907b6bfe2
Liberto Camús <liberto@lcamus.es>
2024-02-20 11:29:55 +0100

    Fix test

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,7 +1,6 @@
 test_fecha() {
 	assertEquals "2024-02-20 15:04:30" "$(scripts/fecha.sh)"
 	assertNotEquals "2024-02-23 15:04:30" "$(scripts/fecha.sh)"
-	assertEquals "2024-01-20 12:00:00" "$(scripts/fecha.sh)"
 }
 
 test_suma() {
