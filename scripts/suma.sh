c8642798f83d4800e204ec3c5dd218166d318f73
Liberto Camús <liberto@lcamus.es>
2024-02-20 11:28:21 +0100

    Improve tests

---
+++ scripts/suma.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+## author: Liberto Camús
+
+# Suma 2 números
+
+expr $1 + $2
+
--- test_scripts.sh
+++ test_scripts.sh
@@ -4,6 +4,10 @@ test_fecha() {
 	assertEquals "2024-01-20 12:00:00" "$(scripts/fecha.sh)"
 }
 
+test_suma() {
+	assertEquals 8 "$(scripts/suma.sh 3 5)"
+}
+
 main() {
 	source shunit2/shunit2
 }
